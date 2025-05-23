b7ea95c53f3e2d5d9168cc49bc0000257663b64e
Chakkrit Chueangyang <him.chakkrit@gmail.com>
2025-05-23 10:53:57 +0700

    Initial Commit

---
+++ ios-webview/ios_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_webviewApp.swift
+//  ios-webview
+//
+//  Created by Chakkrit Chueangyang on 23/5/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct ios_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
